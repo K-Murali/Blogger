880d7043c2b423c6bb45fde7978dc228c0b2aa0c
K-Murali <kmuralidhar888@gmail.com>
2024-08-04 00:11:30 +0530

    done

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 // process.env.REACT_APP_API_KEY
 
-const BASE_URL = "https://notedb.vercel.app/";
+const BASE_URL = "https://notedb.vercel.app";
 // const BASE_URL = "https://notedb.onrender.com";
 // const BASE_URL = "http://localhost:8000";
 const IMAGE_URL = "https://res.cloudinary.com/drfvhp1jh/image/upload";
